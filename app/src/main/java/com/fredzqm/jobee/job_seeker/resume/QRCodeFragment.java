50eaf40ed881625c4127880e4752ffbc5cb858ef
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-08-06 02:09:44 -0500

    QR code scan successful

--- app/src/main/java/com/fredzqm/jobee/job_seeker/resume/QRCodeFragment.java
+++ app/src/main/java/com/fredzqm/jobee/job_seeker/resume/QRCodeFragment.java
@@ -1,15 +1,22 @@
 package com.fredzqm.jobee.job_seeker.resume;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.fredzqm.jobee.ContainedFragment;
 import com.fredzqm.jobee.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,6 +65,24 @@ public class QRCodeFragment extends ContainedFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.js_resume_qrcode_frag, container, false);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.js_qr_image_view);
+        // this is a small sample use of the QRCodeEncoder class from zxing
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(mResumeKey, BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            imageView.setImageBitmap(bmp);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
         return view;
     }
 
